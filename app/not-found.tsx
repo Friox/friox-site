10bdab5493e941ef0e9975ea111be87e0502e590
Friox <ljykdltmdgns624@gmail.com>
2024-04-17 01:57:31 +0900

    init

---
+++ app/not-found.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import React from "react";
+import Particles from "../components/particles";
+import { SlArrowLeftCircle } from "react-icons/sl";
+import styles from './styles.module.css'
+import { useRouter } from 'next/navigation'
+
+export default function Home() {
+  const router = useRouter()
+  return (
+    <div className={`${styles.notfound_body}`}>
+      <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+        <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100}/>
+        <h3 className={`${styles.subtitle} text-2xl text-white/80 font-display`}>devFriox</h3>
+        <h1 className={`${styles.title} mt-2 text-5xl sm:text-6xl md:text-8xl text-white/90 font-display`}>404 Not Found</h1>
+        <h2 className={`${styles.desc} mt-8 text-xl text-white/30`}>I can&apos;t find your destination :(</h2>
+        <div className={`${styles.buttoncase} mt-8`}>
+          <SlArrowLeftCircle size='60px' className={styles.backbutton} onClick={router.back}/>
+        </div>
+      </div>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 const config: Config = {
   content: [
@@ -8,13 +9,82 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      typography: {
+        DEFAULT: {
+          css: {
+            "code::before": {
+              content: '""',
+            },
+            "code::after": {
+              content: '""',
+            },
+          }
+        },
+        quoteless: {
+          css: {
+            "blockquote p:first-of-type::before": { content: "none" },
+            "blockquote p:first-of-type::after": { content: "none" },
+          }
+        }
       },
+      fontFamily: {
+        sans: ["var(--font-inter)", ...defaultTheme.fontFamily.sans],
+		    display: ["var(--font-calsans)"],
+      },
+      backgroundImage: {
+				"gradient-radial": "radial-gradient(50% 50% at 50% 50%, var(--tw-gradient-stops))",
+			},
+			animation: {
+				"fade-in": "fade-in 1s linear forwards",
+				title: "title 1s ease-out forwards",
+				"fade-left": "fade-left 1s linear forwards",
+				"fade-right": "fade-right 1s linear forwards",
+			},
+			keyframes: {
+				"fade-in": {
+					"0%": {
+						opacity: "0%",
+					},
+					"75%": {
+						opacity: "50%",
+					},
+					"100%": {
+						opacity: "100%",
+					},
+				},
+				"fade-left": {
+					"0%": {
+						transform: "translateX(100%)",
+						opacity: "0%",
+					},
+
+					"30%": {
+						transform: "translateX(0%)",
+						opacity: "100%",
+					},
+					"100%": {
+						opacity: "0%",
+					},
+				},
+				"fade-right": {
+					"0%": {
+						transform: "translateX(-100%)",
+						opacity: "0%",
+					},
+
+					"30%": {
+						transform: "translateX(0%)",
+						opacity: "100%",
+					},
+					"100%": {
+						opacity: "0%",
+					},
+				},
+			},
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+  ],
 };
 export default config;
