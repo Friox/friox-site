08fb9eb0d8eb578b6e81a412c4a5d2116eecc1e8
Friox <ljykdltmdgns624@gmail.com>
2024-04-18 02:56:39 +0900

    change nav fade duration

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <div className={`${styles.main_body}`}>
       <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-        <nav className="mb-8 animate-fade-in">
+        <nav className={`${styles.subtitle} mb-8`}>
           <ul className="flex items-center justify-center gap-4">
             {navigation.map((item) => (
               <Link key={item.href} href={item.href} className="text-sm duration-500 text-zinc-500 hover:text-zinc-300">
